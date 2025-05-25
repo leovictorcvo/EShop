da6f2060e84b58ebf92f2d27e4631d3e2c4ab51b
Leonardo Medeiros <leovictorcvo@gmail.com>
2025-05-25 17:14:04 -0300

    Finished Ordering structure using clean architecture

---
+++ src/Services/Ordering/Ordering.Api/DependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿namespace Ordering.Api;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    {
+        return services;
+    }
+
+    public static WebApplication UseApiServices(this WebApplication webApplication)
+    {
+        return webApplication;
+    }
+}
---
+++ src/Services/Ordering/Ordering.Api/Program.cs
@@ -0,0 +1,16 @@
+using Ordering.Api;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices();
+
+var app = builder.Build();
+
+app.UseApiServices();
+
+await app.RunAsync();
